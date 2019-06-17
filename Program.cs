bb8bf6249827836ec1699dcfc80b3b002f7bf839
Gabriel Bernardes Tacconi Lourenço <gabrieltacconi147@gmail.com>
2019-06-17 16:46:01 -0300

    produtos

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace diferencadeProdutos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double valorA, valorB, valorC, valorD, diferença;
+
+
+            Console.WriteLine("DIGITE O PRIMEIRO VALOR");
+            valorA = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("DIGITE O SEGUNDO VALOR");
+            valorB = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("DIGITE O TERCEIRO VALOR");
+            valorC = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("DIGITE O QUARTO VALOR");
+            valorD = double.Parse(Console.ReadLine());
+
+
+            diferença = ((valorA * valorB) - (valorC * valorD));
+
+
+
+
+            Console.WriteLine("DIFERENÇA = " + diferença.ToString("F0", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
